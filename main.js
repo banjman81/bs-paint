cd5723511323c72f425f6c5b0414f9a765fcec5c
[banjman81] <[benjamin.man97@gmail.com]>
2021-07-20 14:46:19 -0500

    v1.0 paint'

--- main.js
+++ main.js
@@ -15,12 +15,12 @@
  * To make the second one happen, the number to change
  * is the first argument to `repeat`, currently set at 10.
  */
-const gridWidth = 10;
+const gridWidth = 75;
 let count = 0;
 while (count <= gridWidth * gridWidth) {
   const canvas = document.querySelector('.canvas');
   const div = document.createElement('div');
-  div.className = 'square color-5';
+  div.className = 'square color-9';
   canvas.appendChild(div);
   count++;
 }
@@ -49,6 +49,10 @@ while (count <= gridWidth * gridWidth) {
 // Add queries for all your squares, palette colors, and brush here.
 // (Note the singular or plural used in that sentence!)
 
+const blocks = document.querySelectorAll('.square');
+const colors = document.querySelectorAll('.palette .palette-color');
+const brush = document.querySelector('.brush-icon');
+
 
 
 /****************************
@@ -60,7 +64,50 @@ while (count <= gridWidth * gridWidth) {
 // empty at first, though a console.log just to know they're being
 // run as event listeners (after the next step is set up) isn't a
 // bad idea for testing purposes.
+let clicked = false
+let colorPicked = 'color-9'
 
+brush.className = `icon palette-icon ${colorPicked}`
+
+colors.forEach(function(color){
+  color.addEventListener('click', function(){
+    const classArr = color.className.split(' ')
+    colorPicked = classArr[classArr.length-1]
+    console.log(colorPicked)
+    // if(color.className == 'palette-color color-1'){
+    //   colorPicked = 'color-1'
+    // }
+    // else if(color.className == 'palette-color color-2'){
+    //   colorPicked = 'color-2'
+    // }
+    // else if(color.className == 'palette-color color-3'){
+    //   colorPicked = 'color-3'
+    // }
+    // else if(color.className == 'palette-color color-4'){
+    //   colorPicked = 'color-4'
+    // }
+    // else if(color.className == 'palette-color color-5'){
+    //   colorPicked = 'color-5'
+    // }
+    brush.className = `icon palette-icon ${colorPicked}`
+  })
+})
+
+blocks.forEach(function(square){
+    square.addEventListener('mousedown', function(){
+      clicked = true
+      console.log(clicked)
+    })
+    square.addEventListener('mouseup', function(){
+      clicked = false
+      console.log(clicked)
+    })
+    square.addEventListener('mouseover', function(){
+      if(clicked == true){
+        square.className = `square ${colorPicked}`
+      }
+    })
+})
 
 
 /**************************
